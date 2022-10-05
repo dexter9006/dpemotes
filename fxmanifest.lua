eb753f26c2b603b0bcaa517d1a77019c05b7daca
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-10-05 17:31:28 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,10 +7,11 @@ fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
 
--- Remove the following lines if you don't want to use the SQL keybinds, requires oxymysql
 --#region oxmysql
 -- dependency 'oxmysql'
 -- server_script "@oxmysql/lib/MySQL.lua"
+dependency 'oxmysql'
+server_script "@oxmysql/lib/MySQL.lua"
 --#endregion oxmysql
 
 shared_scripts {
